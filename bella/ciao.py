e5374fe50745011b278efc49a371429e05e84ce5
Iman Akbari <imakbari@gmail.com>
2019-07-01 18:06:20 -0400

    VNet status return as NumPy

--- bella/ciao.py
+++ bella/ciao.py
@@ -21,6 +21,7 @@ class GemelEnv(gym.Env):
     def __init__(self):
         self.simulations = None
         self.ip_id_map = None
+        self.arp_table = None
         self.known_alerts = None
         self.vnets = None
 
@@ -29,6 +30,9 @@ class GemelEnv(gym.Env):
         return 600
 
     def _fetch_alerts(self):
+        """
+        Fetch IDS alerts and filter out irrelevant one
+        """
 
         ids_alerts = ApiWrapper.get_events(interval=self._interval)
 
@@ -42,6 +46,9 @@ class GemelEnv(gym.Env):
         return dict([(k, v[-MAX_ALERTS_PER_HOST:]) for k, v in obs.items()])
 
     def _get_ids_observations(self):
+        """
+        Get IDS alerts and convert to n-dimensional features array
+        """
 
         # get list of alerts per host
         ids_info = {self.ip_id_map[k]: v for k, v in self._fetch_alerts().items() if k in self.ip_id_map}
@@ -61,9 +68,27 @@ class GemelEnv(gym.Env):
         # convert to NumPy n-dimensional array
         return np.asarray(ids_info)
 
+    def _simulations_sorted_by_id(self):
+        return sorted((host for _, hosts in self.simulations.items() for host in hosts), key=lambda x: x["id"])
+
     def _get_vnet_status(self):
-        vn_status = ApiWrapper.vnet_status()
-        return vn_status
+        """
+        Receives which vn each host is in and returns as
+        feature array
+        """
+
+        # fetch where each host is
+        vnet_status = ApiWrapper.vnet_status()
+
+        # use list of vnet names to assign a "number" to each vnet
+        # (i.e. index of the vnet in the list)
+        vnet_names = [x["name"] for x in self.vnets]
+
+        # get a list of vnet names for each host
+        sorted_list = [vnet_status[host["mac"]] for host in self._simulations_sorted_by_id()]
+
+        # use vnet "number" instead of vnet name and convert to NumPy array
+        return np.asarray([vnet_names.index(name) for name in sorted_list])
 
     def _get_state(self):
         return self._get_vnet_status(), self._get_ids_observations()
@@ -92,6 +117,9 @@ class GemelEnv(gym.Env):
         alerts = ApiWrapper.get_known_alert()
         alerts = sorted(alerts, key=lambda x: x["id"])
 
+        # fetch ARP table
+        self.arp_table = ApiWrapper.get_arp_table()
+
         # fetch vnet list
         self.vnets = ApiWrapper.vnet_list()
 
@@ -100,12 +128,17 @@ class GemelEnv(gym.Env):
         self.ip_id_map = ip_id_map
         self.known_alerts = alerts
 
+    def _get_mac_id(self, mac):
+        for x in ((ip, mac) for ip, macs in self.arp_table.items() for mac in macs):
+            if x[1] == mac:
+                return self.ip_id_map[x[0]]
+        raise Exception(f"MAC address {mac} not found in ARP table")
+
     def _reset_all_hosts_vnet(self):
         """
         Moves all hosts to the initial virtual-net (lowest security)
         """
-        for _, hosts in self.simulations.items():
-            for host in hosts:
+        for host in self._simulations_sorted_by_id():
                 ApiWrapper.set_vnet(host["mac"], self.vnets[0]["name"])
 
     def step(self, action):
