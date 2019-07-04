c594ee96ba5255fb4f316e907b91f071788550bd
Iman Akbari <imakbari@gmail.com>
2019-07-04 14:42:12 -0400

    RL notebook updates

--- exp_001.py
+++ exp_001.py
@@ -14,7 +14,7 @@
 # 
 # **Reward**: XNOR of current VN state and desired state (dumb)
 
-# In[1]:
+# In[14]:
 
 
 from bella.ciao import GemelEnv
@@ -28,13 +28,17 @@ import numpy as np
 from queue import Queue
 import argparse
 import matplotlib.pyplot as plt
+import enum
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, BatchNormalization
 from keras.optimizers import Adam
+from keras.backend import tensorflow_backend as K
+
+from IPython.core.display import display, HTML, clear_output
 
 
-# In[2]:
+# In[5]:
 
 
 EPSILON = 0.1
@@ -42,9 +46,7 @@ EXPLORATION_DECAY = 0.99
 GAMMA = 0.99
 
 
-# ## DQN Agent
-
-# In[3]:
+# In[6]:
 
 
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
@@ -66,16 +68,35 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     # Print New Line on Complete
     if iteration == total:
         print()
+    
+
+
+# ## DQN Agent
+
+# In[9]:
 
 
 class DQNAgent:
+    
+    class StateModel(enum.Enum):
+        VN_ONLY = 1
+        IDS = 2
 
-    def __init__(self, env, max_eps, period=10):
+    def __init__(self, env, max_eps, period=10, 
+                 state_mode=StateModel.IDS, model=None, 
+                 gamma=GAMMA, max_epsilon=EPSILON,
+                 epsilon_decay=EXPLORATION_DECAY):
+        
         self.env = env
         self.max_episodes = max_eps
-        self.model = self._create_model()
-        self.epsilon = EPSILON
+        self.epsilon = max_epsilon
+        self.max_epsilon = max_epsilon
+        self.epsilon_dacay = epsilon_decay
         self.period = period   
+        self.state_mode = state_mode
+        self.gamma = gamma
+        self.model = model or self._create_model()
+        
 
     def _create_model(self):
         """
@@ -83,18 +104,26 @@ class DQNAgent:
         """
         model = Sequential()
         model.add(Dense(
-            20,
-            input_shape=self.env.observation_shape(),
+            5,
+            # input_shape=,
+            input_shape=(len(self.env._hosts_sorted_by_id),) \
+                    if self.state_mode == DQNAgent.StateModel.VN_ONLY \
+                    else self.env.observation_shape(),
             activation="relu"
         ))
-        model.add(Dense(20, activation="relu"))
+        # model.add(Dense(20, activation="relu"))
         model.add(Dense(self.env.action_space.n, activation="linear"))
         model.compile(loss="mse", optimizer=Adam(lr=0.001))
+        model.summary()
         return model
     
-    @staticmethod
-    def _to_feature_vector(state):
-        return np.concatenate((state[0], state[1].flatten()))
+    def _to_feature_vector(self, state):
+        if self.state_mode == DQNAgent.StateModel.VN_ONLY:
+            return state[0]
+        elif self.state_mode == DQNAgent.StateModel.IDS:
+            return np.concatenate((state[0], state[1].flatten()))
+        else:
+            raise Exception()
 
     def train(self):
 
@@ -113,7 +142,7 @@ class DQNAgent:
             state = self.env.state()
                               
             # flatten state
-            state = DQNAgent._to_feature_vector(state)
+            state = self._to_feature_vector(state)
             
             history = []
 
@@ -123,20 +152,21 @@ class DQNAgent:
                 step += 1
                 
                 # pick action based on policy
-                action = self.policy(state)
+                action, is_random = self.policy(state)
                 
                 print()
                 print(f"Taking action {action}")
 
                 # run action and get reward
-                _, reward, terminal = self.env.step(action)
+                state_next_raw, reward, terminal = self.env.step(action)
                 
                 # instead of using the immediate next state, wait for it to simmer
-                time.sleep(self.period)
-                state_next_raw = self.env.state()
-                              
+                if self.period > 0:
+                    time.sleep(self.period)
+                    state_next_raw = self.env.state()
+
                 # flatten state
-                state_next = DQNAgent._to_feature_vector(state_next_raw)
+                state_next = self._to_feature_vector(state_next_raw)
                 
                 print()
                 print(f"Step {step} reward={reward} new_state={state_next_raw}")
@@ -145,9 +175,14 @@ class DQNAgent:
                 # # where a terminal state means "losing"
                 # if terminal:
                 #    reward *= -1
+                
+                preds = self.model.predict([[state_next]])
+                next_scores_prediction = preds[0]
+                
+                print(f"Predicted scores for each action in next step: {next_scores_prediction}")
 
                 # compute target Q
-                q_target = ( reward + GAMMA * np.amax(self.model.predict([[state_next]])[0]) )                         if not terminal else reward
+                q_target = ( reward + self.gamma * np.amax(next_scores_prediction) )                         if not terminal else reward
 
                 # update model
                 q_updated = self.model.predict([[state]])[0]
@@ -162,23 +197,25 @@ class DQNAgent:
                     "time": step,
                     "action": action,
                     "reward": reward,
-                    "state": state_next_raw[0].tolist()
+                    "state": state_next_raw[0].tolist(),
+                    "random": is_random,
+                    "prediction": preds,
                 })
 
             histories.append(history)
             
             # apply exploration decay
-            self.epsilon *= EXPLORATION_DECAY
+            self.epsilon *= self.epsilon_dacay
 
         return histories
 
     def policy(self, state):
         if np.random.rand() < self.epsilon:
             print("PERFORMING RANDOM ACTION")
-            return np.random.randint(self.env.action_space.n)
+            return np.random.randint(self.env.action_space.n), True
         else:
             expected_rewards = self.model.predict([[state]])[0]
-            return np.argmax(expected_rewards)
+            return np.argmax(expected_rewards), False
 
     def test(self):
 
@@ -198,18 +235,540 @@ class DQNAgent:
         print(f"Total reward: {total_reward}")
 
 
-# ## Running DQN
+# In[30]:
+
+
+def create_model_1(env):
+    """
+    Builds a neural net model to digest the state
+    """
+    model = Sequential()
+    model.add(Dense(
+        5,
+        # input_shape=,
+        input_shape=(2,),
+        activation="relu"
+    ))
+    model.add(Dense(20, activation="relu"))
+    model.add(Dense(env.action_space.n, activation="linear"))
+    model.compile(loss="mse", optimizer=Adam(lr=0.001))
+    model.summary()
+    return model
+
+
+# # Running DQN
+
+# ## Experiment 001 
+# 
+# State: just where each host is
+# Model: 3 dense layers
+# 
+# ![zena](https://i.imgflip.com/2fw0fb.jpg)
+
+# In[4]:
+
+
+env = GemelEnv(interval=10, max_steps=500)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=5, state_mode=DQNAgent.StateModel.VN_ONLY, model=create_model_1(env))
+hist = agent.train()
+
+hist
+
+
+# In[5]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# ## Experiment 002
+
+# smaller middle layer
+
+# In[28]:
+
+
+def create_model_2(env):
+    model = Sequential()
+    model.add(Dense(
+        5,
+        input_shape=(len(env._hosts_sorted_by_id),),
+        activation="relu"
+    ))
+    model.add(Dense(env.action_space.n, activation="linear"))
+    model.compile(loss="mse", optimizer=Adam(lr=0.001))
+    model.summary()
+    return model
+
+
+# In[24]:
+
+
+env = GemelEnv(interval=10, max_steps=500)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=5, state_mode=DQNAgent.StateModel.VN_ONLY, model=create_model_2(env))
+hist = agent.train()
+
+hist
+
+
+# ## Experiment 003
+
+# Is it about not having enough time? Let's find out. 
+# 
+# Forget about IDS feedback. instaed of letting it soak we would just get the fake reward and let it continue.
+
+# In[33]:
+
+
+env = GemelEnv(interval=10, max_steps=500)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=0, state_mode=DQNAgent.StateModel.VN_ONLY, model=create_model_2(env))
+hist = agent.train()
+
+hist
+
+
+# In[34]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# ## Experiment 004
+
+# In[39]:
+
+
+def create_model_4(env):
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=(len(env._hosts_sorted_by_id),)))
+    model.add(Dense(5, activation="relu"))
+    model.add(Dense(5, activation="relu"))
+    model.add(Dense(env.action_space.n, activation="linear"))
+    model.compile(loss="mse", optimizer=Adam(lr=0.01))
+    model.summary()
+    return model
+
+
+# In[42]:
+
+
+env = GemelEnv(interval=10, max_steps=500)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=0, state_mode=DQNAgent.StateModel.VN_ONLY, model=create_model_3(env))
+hist = agent.train()
+
+hist
+
+
+# In[43]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# ## Experiment 005
+
+# In[46]:
+
+
+def create_model_5(env):
+    model = Sequential()
+#     model.add(Dense(
+#         5,
+#         input_shape=(len(env._hosts_sorted_by_id),),
+#         activation="relu"
+#     ))
+    model.add(Dense(env.action_space.n, input_shape=(len(env._hosts_sorted_by_id),), activation="linear"))
+    model.compile(loss="mse", optimizer=Adam(lr=0.01))
+    model.summary()
+    return model
+
+
+# In[47]:
+
+
+env = GemelEnv(interval=10, max_steps=500)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=0, state_mode=DQNAgent.StateModel.VN_ONLY, model=create_model_4(env))
+hist = agent.train()
+
+hist
+
+
+# In[50]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# ## Experiment 006
+# 
+# EXP-005 + Batch Normalization
+
+# In[58]:
+
+
+def create_model_6(env):
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=(len(env._hosts_sorted_by_id),)))
+#     model.add(Dense(
+#         5,
+#         input_shape=(len(env._hosts_sorted_by_id),),
+#         activation="relu"
+#     ))
+    model.add(Dense(env.action_space.n, activation="linear"))
+    model.compile(loss="mse", optimizer=Adam(lr=0.01))
+    model.summary()
+    return model
+
+
+# In[59]:
+
+
+env = GemelEnv(interval=10, max_steps=200)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=0, state_mode=DQNAgent.StateModel.VN_ONLY, model=create_model_6(env))
+hist = agent.train()
+
+hist
+
+
+# In[67]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# ## Experiment 007
+
+# Trying to fix the oscillations by tuning gamma
+
+# In[68]:
+
+
+def create_model_7(env):
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=(len(env._hosts_sorted_by_id),)))
+
+    model.add(Dense(env.action_space.n, activation="linear"))
+    model.compile(loss="mse", optimizer=Adam(lr=0.01))
+    model.summary()
+    return model
+
+
+# In[71]:
+
+
+env = GemelEnv(interval=10, max_steps=200)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=0, state_mode=DQNAgent.StateModel.VN_ONLY, gamma=0.5, model=create_model_7(env))
+hist = agent.train()
+
+hist
+
+
+# In[73]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# ## Experiment 008
+
+# In[85]:
+
+
+env = GemelEnv(interval=10, max_steps=100)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=0, state_mode=DQNAgent.StateModel.VN_ONLY, gamma=0.1, model=create_model_7(env))
+hist = agent.train()
+
+hist
+
+
+# We can see that it's a tat too slow in learning bad actions (see steps ~20)
+
+# In[86]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# # Experiment 009
+
+# In[87]:
+
+
+def create_model_9(env):
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=(len(env._hosts_sorted_by_id),)))
+
+    model.add(Dense(env.action_space.n, activation="linear"))
+    model.compile(loss="mse", optimizer=Adam(lr=0.1))
+    model.summary()
+    return model
+
+
+# In[90]:
+
+
+env = GemelEnv(interval=10, max_steps=100)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=0, state_mode=DQNAgent.StateModel.VN_ONLY, gamma=0.8, model=create_model_9(env))
+hist = agent.train()
+
+hist
+
+
+# In[91]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# It seems like the single layer model does not have the capacity to learn toggles easily. it's always toggling between two states for solving such a simplistic set-up.
+# 
+# Also, the "toggling" scheme seemed at first like a clever trick to contorl the action-space but it's wasting a lot of training time to be learned.
+
+# ## Experiment 010
+
+# In[10]:
+
+
+def create_model_10(env):
+    model = Sequential()
+    # model.add(BatchNormalization(input_shape=(len(env._hosts_sorted_by_id),)))
+
+    model.add(Dense(
+        4, 
+        activation="relu",
+        input_shape=(len(env._hosts_sorted_by_id),)
+    ))
+    
+    model.add(Dense(
+        env.action_space.n,
+        activation="linear",
+    ))
+    
+    model.compile(loss="mse", optimizer=Adam(lr=0.01))
+    model.summary()
+    
+    return model
+
+
+# In[12]:
+
+
+env = GemelEnv(interval=10, max_steps=100)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=0, state_mode=DQNAgent.StateModel.VN_ONLY, gamma=0.8, model=create_model_10(env))
+hist = agent.train()
+
+hist
+
+
+# In[13]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# In[16]:
+
+
+K.set_value(agent.model.optimizer.lr, 0.001)
+hist = agent.train()
+
+
+# In[17]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# In[20]:
+
+
+K.set_value(agent.model.optimizer.lr, 0.0001)
+hist = agent.train()
+
+
+# In[21]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# In[25]:
+
+
+agent.env.max_steps = 500
+hist = agent.train()
+
+
+# In[26]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# The oscillations are ameliorated but still, the model seems clueless about what it is doing
+
+# ## Experiment 11
+
+# In[27]:
+
+
+def create_model_11(env):
+    model = Sequential()
+    # model.add(BatchNormalization(input_shape=(len(env._hosts_sorted_by_id),)))
+
+    model.add(Dense(
+        4, 
+        activation="relu",
+        input_shape=(len(env._hosts_sorted_by_id),)
+    ))
+    
+    model.add(Dense(4, activation="relu"))
+    
+    model.add(Dense(
+        env.action_space.n,
+        activation="linear",
+    ))
+    
+    model.compile(loss="mse", optimizer=Adam(lr=0.01))
+    model.summary()
+    
+    return model
+
+
+# In[28]:
+
+
+env = GemelEnv(interval=10, max_steps=500)
+env.reset()
+
+agent = DQNAgent(env, max_eps=1, period=0, state_mode=DQNAgent.StateModel.VN_ONLY, gamma=0.8, model=create_model_11(env))
+hist = agent.train()
+
+hist
+
+
+# In[29]:
+
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# In[30]:
+
+
+K.set_value(agent.model.optimizer.lr, 0.001)
+hist = agent.train()
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# In[39]:
+
+
+ticks = [idx for idx, x in enumerate(hist[0]) if x["random"]]
+
+for xc in ticks:
+    plt.axvline(x=xc, color='y')
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# Congrats! the model has learned the concept of "toggles". 
+# 
+# ### Why Toggles?
+# 
+# A good approach would just have a more-security and a less-security button for each host, this is more in line with the multi (>2) VN approach and it wouldn't burden the model unnecessarily.
+# 
+# Let's let the model run for a few more epochs at finer grain steps.
+
+# In[40]:
+
+
+K.set_value(agent.model.optimizer.lr, 0.0001)
+hist = agent.train()
+
+
+# In[41]:
+
+
+ticks = [idx for idx, x in enumerate(hist[0]) if x["random"]]
+
+for xc in ticks:
+    plt.axvline(x=xc, color='y')
+
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# ## Experiment 012
+
+# In[42]:
+
+
+def create_model_12(env):
+    model = Sequential()
+    model.add(Dense(4, activation="relu", input_shape=env.observation_shape()))
+    model.add(Dense(4, activation="relu"))
+    model.add(Dense(env.action_space.n, activation="linear"))
+    model.compile(loss="mse", optimizer=Adam(lr=0.01))
+    model.summary()    
+    return model
+
+
+# In[44]:
+
+
+env = GemelEnv(interval=10, max_steps=200)
+env.reset()
+env.observation_shape()
+
+# agent = DQNAgent(env, max_eps=1, period=5, state_mode=DQNAgent.StateModel.IDS, gamma=0.8, model=create_model_11(env))
+# hist = agent.train()
+
+# hist
+
 
 # In[ ]:
 
 
-env = GemelEnv(interval=10, max_steps=40)
-env.reset()
+ticks = [idx for idx, x in enumerate(hist[0]) if x["random"]]
 
-agent = DQNAgent(env, max_eps=10, period=5)
-history = agent.train()
+for xc in ticks:
+    plt.axvline(x=xc, color='y')
 
-history
+plt.plot([x['reward'] for x in hist[0]])
+
+
+# <br/>
+
+# In[ ]:
+
+
+display(HTML("""<img width="300px" src="https://i.pinimg.com/originals/04/fa/41/04fa41ddc000ba75de7bce263b8ac469.jpg" />"""))
+
+time.sleep(7)
+
+clear_output()
+
+print("Done.")
 
 
 # In[ ]:
